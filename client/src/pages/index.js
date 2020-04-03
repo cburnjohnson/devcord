c0c5ba9842ba66ea8476f99ed05da61df551fe12
Cole Johnson <cburnjohnson@gmail.com>
2020-04-03 15:44:09 -0700

    initial styling

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,6 +8,30 @@ import SEO from '../components/seo';
 const IndexPage = () => (
     <Layout>
         <SEO title='Home' />
+        <div id='chat-container'>
+            <div id='chat-list'>
+                <h2>Users in Room</h2>
+                <ul>
+                    <li>cole</li>
+                    <li>bob</li>
+                    <li>kam</li>
+                    <li>kurt</li>
+                    <li>ricky</li>
+                </ul>
+            </div>
+            <div id='chat-section'></div>
+            <div id='chat-form'>
+                <form>
+                    <input
+                        type='text'
+                        name='message'
+                        id='message'
+                        placeholder='Enter message...'
+                    />
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        </div>
     </Layout>
 );
 
