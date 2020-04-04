4d0cefdf1b104901c0727a7ddfaa905633e73bd9
Cole Johnson <cburnjohnson@gmail.com>
2020-04-04 14:41:08 -0700

    style update

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -19,7 +19,80 @@ const IndexPage = () => (
                     <li>ricky</li>
                 </ul>
             </div>
-            <div id='chat-section'></div>
+            <div id='chat-section'>
+                <div className='message'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita dicta soluta vero atque sint et itaque tempore?
+                        Laboriosam asperiores iure, explicabo perspiciatis quasi
+                        quaerat aspernatur. Aliquid numquam neque consequuntur
+                        nihil exercitationem sed dicta esse quaerat odio
+                        possimus blanditiis cupiditate rerum voluptates harum,
+                        sequi culpa. Assumenda voluptatibus perspiciatis labore
+                        possimus soluta?
+                    </p>
+                </div>
+                <div className='message'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita dicta soluta vero atque sint et itaque tempore?
+                        Laboriosam asperiores iure, explicabo perspiciatis quasi
+                        quaerat aspernatur. Aliquid numquam neque consequuntur
+                        nihil exercitationem sed dicta esse quaerat odio
+                        possimus blanditiis cupiditate rerum voluptates harum,
+                        sequi culpa. Assumenda voluptatibus perspiciatis labore
+                        possimus soluta?
+                    </p>
+                </div>
+                <div className='message'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita dicta soluta vero atque sint et itaque tempore?
+                        Laboriosam asperiores iure, explicabo perspiciatis quasi
+                        quaerat aspernatur. Aliquid numquam neque consequuntur
+                        nihil exercitationem sed dicta esse quaerat odio
+                        possimus blanditiis cupiditate rerum voluptates harum,
+                        sequi culpa. Assumenda voluptatibus perspiciatis labore
+                        possimus soluta?
+                    </p>
+                </div>
+                <div className='message'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita dicta soluta vero atque sint et itaque tempore?
+                        Laboriosam asperiores iure, explicabo perspiciatis quasi
+                        quaerat aspernatur. Aliquid numquam neque consequuntur
+                        nihil exercitationem sed dicta esse quaerat odio
+                        possimus blanditiis cupiditate rerum voluptates harum,
+                        sequi culpa. Assumenda voluptatibus perspiciatis labore
+                        possimus soluta?
+                    </p>
+                </div>
+                <div className='message'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita dicta soluta vero atque sint et itaque tempore?
+                        Laboriosam asperiores iure, explicabo perspiciatis quasi
+                        quaerat aspernatur. Aliquid numquam neque consequuntur
+                        nihil exercitationem sed dicta esse quaerat odio
+                        possimus blanditiis cupiditate rerum voluptates harum,
+                        sequi culpa. Assumenda voluptatibus perspiciatis labore
+                        possimus soluta?
+                    </p>
+                </div>
+                <div className='message'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita dicta soluta vero atque sint et itaque tempore?
+                        Laboriosam asperiores iure, explicabo perspiciatis quasi
+                        quaerat aspernatur. Aliquid numquam neque consequuntur
+                        nihil exercitationem sed dicta esse quaerat odio
+                        possimus blanditiis cupiditate rerum voluptates harum,
+                        sequi culpa. Assumenda voluptatibus perspiciatis labore
+                        possimus soluta?
+                    </p>
+                </div>
+            </div>
             <div id='chat-form'>
                 <form>
                     <input
