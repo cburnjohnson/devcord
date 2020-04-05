b2acbe66fbde234fb3cc5ba19840b48e32690d3a
Cole Johnson <cburnjohnson@gmail.com>
2020-04-05 10:48:10 -0700

    initial join room form

--- client/src/pages/joinroom.js
+++ client/src/pages/joinroom.js
@@ -3,9 +3,21 @@ import Layout from '../components/layout';
 
 const JoinRoom = () => {
     return (
-        <div className='container'>
-            <div>Join Room</div>
-        </div>
+        <Layout>
+            <form className='joinroom-form'>
+                <label htmlFor='username'>Username</label>
+                <input type='text' id='username' />
+                <select name='rooms' id='rooms'>
+                    <option value='javascript'>JavaScript</option>
+                    <option value='python'>Python</option>
+                    <option value='c#'>C#</option>
+                    <option value='java'>Java</option>
+                    <option value='php'>PHP</option>
+                    <option value='ruby'>Ruby</option>
+                    <option value='c'>C</option>
+                </select>
+            </form>
+        </Layout>
     );
 };
 
