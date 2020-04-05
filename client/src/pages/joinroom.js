7da1c9de07ae841b7ef68b5b3e0eacb94879cba4
Cole Johnson <cburnjohnson@gmail.com>
2020-04-05 10:43:49 -0700

    join room page

---
+++ client/src/pages/joinroom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const JoinRoom = () => {
+    return (
+        <div className='container'>
+            <div>Join Room</div>
+        </div>
+    );
+};
+
+export default JoinRoom;
