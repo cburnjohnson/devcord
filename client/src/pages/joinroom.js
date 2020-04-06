12a9abfc90fecd5702f6be71f82f511d38583af9
Cole Johnson <cburnjohnson@gmail.com>
2020-04-06 15:28:11 -0700

    joinroom styling

--- client/src/pages/joinroom.js
+++ client/src/pages/joinroom.js
@@ -14,15 +14,18 @@ const JoinRoom = () => {
                     />
                 </div>
 
-                <select name='rooms' id='rooms'>
-                    <option value='javascript'>JavaScript</option>
-                    <option value='python'>Python</option>
-                    <option value='c#'>C#</option>
-                    <option value='java'>Java</option>
-                    <option value='php'>PHP</option>
-                    <option value='ruby'>Ruby</option>
-                    <option value='c'>C</option>
-                </select>
+                <div className='form-control'>
+                    <label htmlFor='rooms'>Available rooms to join</label>
+                    <select name='rooms' id='rooms'>
+                        <option value='javascript'>JavaScript</option>
+                        <option value='python'>Python</option>
+                        <option value='c#'>C#</option>
+                        <option value='java'>Java</option>
+                        <option value='php'>PHP</option>
+                        <option value='ruby'>Ruby</option>
+                        <option value='c'>C</option>
+                    </select>
+                </div>
             </form>
         </Layout>
     );
