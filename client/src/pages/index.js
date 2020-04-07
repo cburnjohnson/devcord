8f6e237b254936e0c907744b4c73e58812d7c917
Cole Johnson <cburnjohnson@gmail.com>
2020-04-07 15:20:49 -0700

    refactored

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 import ChatList from '../components/homepage/ChatList';
 import ChatSection from '../components/homepage/ChatSection';
