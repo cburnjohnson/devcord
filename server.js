4be2d74b85024cf7ad6ab51daf334283c54cc9fa
Cole Johnson <cburnjohnson@gmail.com>
2020-04-08 13:33:37 -0700

    setting static assets

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// set static folder
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/public'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
