ac487b4ad2ec304cf53883dccb5928dc6309cf8f
Cole Johnson <cburnjohnson@gmail.com>
2020-04-10 12:45:49 -0700

    added keys to messages

--- client/src/components/homepage/ChatSection.js
+++ client/src/components/homepage/ChatSection.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Message from './Message';
+import { uuid } from 'uuidv4';
 
 const ChatSection = ({ messages }) => {
     return (
         <div id='chat-section'>
-            {messages && messages.map(message => <Message message={message} />)}
+            {messages &&
+                messages.map(message => (
+                    <Message key={uuid()} message={message} />
+                ))}
         </div>
     );
 };
