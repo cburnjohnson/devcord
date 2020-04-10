b63c523d7658a53380282bcf960fdbf8a4372928
Cole Johnson <cburnjohnson@gmail.com>
2020-04-10 16:25:20 -0700

    clean up

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import io from 'socket.io-client';
 
 import Layout from '../components/layout';
