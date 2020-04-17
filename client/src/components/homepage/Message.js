bcf6d54db147445101075f55902e7cb85033025d
Cole Johnson <cburnjohnson@gmail.com>
2020-04-17 14:56:51 -0700

    refactored

--- client/src/components/homepage/Message.js
+++ client/src/components/homepage/Message.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Message = ({ message }) => {
-    console.log(message);
     return (
         <div className='message'>
             <p className='message-details'>
